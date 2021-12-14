28fd117e3c823a65a51a75d7dfd4f845098573d3
Marco Kang <kmnkit@gmail.com>
2021-12-14 20:32:23 +0900

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Sungwoong Kang on 2021/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
